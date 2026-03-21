6658886b8e3153fa4c6297474dd94e28868bb7ad
Antti Kivi <antti.kivi@visiosto.fi>
2026-03-21 18:09:44 +0200

    chore: license the project under AGPL-3.0-or-later

---
+++ main.go
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: © 2026 Visiosto oy <visiosto@visiosto.fi>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
